5770a6892926193dd7c8473883abef44aea9165f
Razumov Igor <74genesis@gmail.com>
2023-04-01 15:55:22 +0400

    Updates

---
+++ assets/global.3e475005.js
@@ -0,0 +1,1 @@
+import{D as e}from"./entry.84fb686f.js";const l=e(()=>{console.log("running global middleware")});export{l as default};
--- assets/global.7c8aec83.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./entry.d6189852.js";const l=e(()=>{console.log("running global middleware")});export{l as default};
