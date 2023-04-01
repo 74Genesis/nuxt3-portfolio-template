559de7c1164923f3017454a225a015786cbcb0cb
Razumov Igor <74genesis@gmail.com>
2023-04-01 15:07:29 +0400

    Updates

--- _nuxt/global.410f2529.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./entry.2ece69a8.js";const l=e(()=>{console.log("running global middleware")});export{l as default};
---
+++ assets/global.5e518f00.js
@@ -0,0 +1,1 @@
+import{C as e}from"./entry.07a65a5e.js";const l=e(()=>{console.log("running global middleware")});export{l as default};
